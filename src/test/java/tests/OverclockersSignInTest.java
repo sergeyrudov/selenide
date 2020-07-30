7c38d15f7f8673fcc562feb3028338a3ea2db6bc
sergeyrudov <255400warr>
2020-07-30 23:20:08 +0300

    - overclockers.test added

---
+++ src/test/java/tests/OverclockersSignInTest.java
@@ -0,0 +1,42 @@
+package tests;
+
+import com.codeborne.selenide.Condition;
+import com.codeborne.selenide.Configuration;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
+import resources.Credentials;
+
+import static com.codeborne.selenide.Selenide.*;
+import static com.codeborne.selenide.WebDriverRunner.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+
+class OverclockersSignInTest {
+    @Test
+    void overclockersSignIn(){
+        //create object with credentials
+        Credentials credentials = new Credentials();
+        Configuration.holdBrowserOpen = true;
+
+        //open overclockers.ua
+        open("https://forum.overclockers.ua");
+
+        //open login window
+        $(".dropdown-toggle").click();
+
+        //sign in flow
+        $(By.id("username_q")).setValue(credentials.getUsername());
+        $(By.id("password_q")).setValue(credentials.getPass());
+        $(By.name("login")).click();
+
+        //ensure that username visible
+        $(By.className("username")).shouldBe(Condition.visible);
+
+        //assert that credentials.username == visible username
+        assertEquals($(By.className("username")).getText(), credentials.getUsername());
+
+        //get url sessionid and ensure that sid!=null
+        assertNotNull(url().split("sid=")[1]);
+    }
+}
