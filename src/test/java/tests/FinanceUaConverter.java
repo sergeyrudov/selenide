42cf62b999c199ea4a6ec2413d9b93e14c115346
sergeyrudov <255400warr>
2020-07-30 20:58:38 +0300

    - financeCalc added

--- src/test/java/tests/FinanceUaConverter.java
+++ src/test/java/tests/FinanceUaConverter.java
@@ -1,12 +1,8 @@
 package tests;
 
-import com.codeborne.selenide.Configuration;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.By;
 import resources.Credentials;
-
-import static com.codeborne.selenide.Condition.text;
-import static com.codeborne.selenide.Selectors.byName;
 import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.open;
 
