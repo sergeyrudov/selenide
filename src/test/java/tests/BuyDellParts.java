d8dc05aea617f7831ac8103e93ab424525787c53
Vasenkov Stanislav <aanher@gmail.com>
2020-08-07 20:13:10 +0300

    Update BuyDellParts.java
    code review

--- src/test/java/tests/BuyDellParts.java
+++ src/test/java/tests/BuyDellParts.java
@@ -18,13 +18,13 @@ class BuyDellParts {
         //find search field, and fill with laptop model
         $(By.className("mh-search-input"))
                 .setValue(Credentials.getNoteBookModel());
-        $(By.className("mh-search-button-label")).click();
+        $(".mh-search-button-label").click();
 
         // we can select needeed parts, depending on request input
-        if (Credentials.getNoteBooksNeededPart()
-                .equals("Battery")) { selectCheckboxBattery();
-        } else if (Credentials.getNoteBooksNeededPart()
-                .equals("Power Adapter")) { selectCheckboxPowerAdapter();
+        if (Credentials.getNoteBooksNeededPart().equals("Battery")) { 
+            selectCheckboxBattery();
+        } else if (Credentials.getNoteBooksNeededPart().equals("Power Adapter")) { 
+            selectCheckboxPowerAdapter();
         } else {
             throw new Exception(Credentials.getNoteBooksNeededPart() + " wasn't find");
         }
@@ -32,14 +32,9 @@ class BuyDellParts {
 
     //method which select battery checkbox
     static void selectCheckboxBattery() {
-        try {
-            $(By.xpath("//span[contains(text(),'Notebook Battery')]")).shouldBe(visible).click();
-            $(By.partialLinkText("Battery")).click();
-            $(By.partialLinkText("Add to Ca")).click();
-        } catch (NoSuchElementException e) {
-            System.out.println(Credentials.getNoteBooksNeededPart() +
-                    " part for laptop is absent");
-        }
+         $(withText("Notebook Battery")).click();
+         $(byPartialLinkText("Battery")).click();
+         $(byPartialLinkText("Add to Ca")).click();
     }
 
     //method which select AC power adapter checkbox
