6aee9ff62e4824a340693a5748b2d441ed5ae7eb
sergeyrudov <255400warr>
2020-08-09 16:02:17 +0300

    - added alfabank

--- src/test/java/tests/UploadFileToFileServer.java
+++ src/test/java/tests/UploadFileToFileServer.java
@@ -1,37 +1,20 @@
 package tests;
 
-import com.codeborne.selenide.Browser;
 import com.codeborne.selenide.Configuration;
-import com.codeborne.selenide.Selenide;
 import org.junit.jupiter.api.Test;
-import org.openqa.selenium.By;
-import org.openqa.selenium.JavascriptExecutor;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
-import org.openqa.selenium.chrome.ChromeDriver;
-
-import java.awt.*;
-import java.awt.datatransfer.StringSelection;
-import java.awt.event.KeyEvent;
 import java.io.File;
 
 import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.open;
 
+
 class UploadFileToFileServer {
     @Test
     void uploadSomeFile(){
-    Configuration.holdBrowserOpen = true;
-
-    File file = new File("F:\\selenide\\src\\test\\java\\resources\\enot.jpg");
-
+    File file = new File("src\\test\\java\\resources\\enot.jpg");
     open("https://ru.files.fm/");
-    $(By.id("uploadifive-file_upload")).uploadFile(file);;
-
-
-      //  File file = new File("F:/selenide/src/test/java/resources/enot.jpg");
-      //  $("#file").input(file);
-
+    $("#uploadifive-file_upload input", 1).uploadFile(file);
+    $("#savefiles").click();
+    $(".upload_button").click();
     }
-
 }
