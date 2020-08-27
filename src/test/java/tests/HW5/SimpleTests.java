d647b4e55ac1f72dff061ee901a0692210bda356
sergeyrudov <255400warr>
2020-08-27 16:15:01 +0300

    - added simple_test

--- src/test/java/tests/HW5/SimpleTests.java
+++ src/test/java/tests/HW5/SimpleTests.java
@@ -1,27 +1,40 @@
 package tests.HW5;
 
+import io.qameta.allure.Feature;
+import io.qameta.allure.Owner;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
 
+import static io.qameta.allure.Allure.*;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 
 @Tag("simple_tests")
-public class SimpleTests {
+@Owner("SergeyR")
+@Feature("working with parametrized tests")
+class SimpleTests {
 
     @Test
+    @DisplayName("Multiply check")
     void parametrizedTest() {
         int a = 3;
-        String b = System.getProperty("value");  //from terminal, gradle simple_tests -Dvalue=5
-        int intValue = Integer.parseInt(b);
+        int intValue = Integer.parseInt(System.getProperty("value")); //from terminal, gradle simple_tests -Dvalue=5
 
-        assertEquals(a * intValue,  15);
+        step("verify result of multiply", () -> {
+            parameter("input value = ", intValue);
+            parameter("result = ", intValue * a);
+            assertEquals(a * intValue,  15);
+        });
     }
 
     @Test
+    @DisplayName("String input check")
     void parametrizedStringTest() {
-        String b = System.getProperty("text");  //from terminal, gradle apple_tests -Dtext=apple
+        String b = System.getProperty("text");  //from terminal, gradle simple_tests -Dtext=apple
 
-        assertEquals("apple", b, "b = " + b);
+        step("string input check", () -> {
+            assertEquals("apple", b, "b = " + b);
+        });
     }
 }
