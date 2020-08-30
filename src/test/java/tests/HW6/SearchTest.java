7a8b5f8cf0e95337a9541f36a4f269656e6256ee
sergeyrudov <255400warr>
2020-08-30 23:39:14 +0300

    - added simple_test

--- src/test/java/tests/HW6/SearchTest.java
+++ src/test/java/tests/HW6/SearchTest.java
@@ -5,22 +5,25 @@ import com.codeborne.selenide.logevents.SelenideLogger;
 import io.qameta.allure.Feature;
 import io.qameta.allure.Owner;
 import io.qameta.allure.selenide.AllureSelenide;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
 import pages.GooglePage;
 
+import static com.codeborne.selenide.Selenide.open;
 import static io.qameta.allure.Allure.step;
 
 @Tag("run_test_in_browser")
 @Owner("SergeyR")
 @Feature("Running test in browser")
-public class SearchTest {
+public class SearchTest extends TestBase{
 
     private GooglePage googlePage = new GooglePage();
     private String seachText = "Привет";
 
     @Test
-    void selenideSearchTest() {
+    @DisplayName("Find text in google chrome browser")
+    void positiveTestSearch() {
         Configuration.headless = true;
         SelenideLogger.addListener("allureSelenide", new AllureSelenide()
                 .screenshots(true)
@@ -37,6 +40,14 @@ public class SearchTest {
 
         step("Verify value", () -> {
             googlePage.verify(seachText);
+        });
+    }
+
+    @Test
+    @DisplayName("Open jenkins job by unsigned user")
+    void negativeJenkinsTest() {
+        step("Open jenkins job", () -> {
+            open("https://jenkins.autotests.cloud/view/Group%203/job/sergeyr_testInBrowser/");
         });
     }
 }
---
+++ src/test/java/tests/HW6/TestBase.java
@@ -0,0 +1,38 @@
+package tests.HW6;
+
+import com.codeborne.selenide.Configuration;
+import com.codeborne.selenide.Selenide;
+import io.qameta.allure.Step;
+import io.qameta.allure.selenide.AllureSelenide;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+
+import static com.codeborne.selenide.Selenide.closeWebDriver;
+import static com.codeborne.selenide.logevents.SelenideLogger.addListener;
+import static io.qameta.allure.selenide.LogType.BROWSER;
+import static tests.HW6.helpers.AttachmentsHelper.*;
+
+
+public class TestBase {
+    @BeforeAll
+    @Step("Tests setup")
+    public static void beforeAll() {
+        addListener("AllureSelenide", new AllureSelenide().screenshots(true).savePageSource(true));
+        Configuration.headless = true;
+    }
+
+    @AfterEach
+    @Step("Attachments")
+    public void afterEach() {
+        attachScreenshot("Last screenshot");
+        attachPageSource();
+        attachAsText("Browser console logs", getConsoleLogs());
+
+        closeWebDriver();
+    }
+
+    public static String getConsoleLogs() {
+        return String.join("\n", Selenide.getWebDriverLogs(String.valueOf(BROWSER)));
+    }
+
+}
