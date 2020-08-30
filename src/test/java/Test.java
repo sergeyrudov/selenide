b0c919a389e2c0485d113ce0f1971bbfc1a067e5
sergeyrudov <255400warr>
2020-08-30 22:29:47 +0300

    - added simple_test

---
+++ src/test/java/Test.java
@@ -0,0 +1,9 @@
+class Test {
+    public static void main(String[] args) {
+        if(4 + 4 == 10 && 24 / 6 == 4 || 4 == 10 % 6){
+            System.out.println("ok");
+        } else {
+            System.out.println("test");
+        }
+    }
+    }
---
+++ src/test/java/tests/HW6/SearchTest.java
@@ -0,0 +1,40 @@
+package tests.HW6;
+
+import com.codeborne.selenide.logevents.SelenideLogger;
+import io.qameta.allure.Feature;
+import io.qameta.allure.Owner;
+import io.qameta.allure.selenide.AllureSelenide;
+import org.junit.jupiter.api.Tag;
+import org.junit.jupiter.api.Test;
+import pages.GooglePage;
+
+import static io.qameta.allure.Allure.step;
+
+@Tag("run_test_in_browser")
+@Owner("SergeyR")
+@Feature("Running test in browser")
+public class SearchTest {
+
+    private GooglePage googlePage = new GooglePage();
+    private String seachText = "Привет";
+
+    @Test
+    void selenideSearchTest() {
+        SelenideLogger.addListener("allureSelenide", new AllureSelenide()
+                .screenshots(true)
+                .savePageSource(true)
+        );
+
+        step("Open google", () -> {
+            googlePage.openMainPage();
+        });
+
+        step("Input searchtext", () -> {
+            googlePage.typeSearch(seachText);
+        });
+
+        step("Verify value", () -> {
+            googlePage.verify(seachText);
+        });
+    }
+}
