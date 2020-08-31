9eedb573c18b05a6df6e206ad5c2d992f5bae0f1
sergeyrudov <255400warr>
2020-08-31 08:46:40 +0300

    - added simple_test

--- src/test/java/tests/HW6/SearchTests.java
+++ src/test/java/tests/HW6/SearchTests.java
@@ -10,11 +10,9 @@ import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
 import pages.GooglePage;
 
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
-
 import static com.codeborne.selenide.Selenide.open;
 import static io.qameta.allure.Allure.step;
+import static org.junit.jupiter.api.Assertions.assertFalse;
 
 @Tag("run_test_in_browser")
 @Owner("SergeyR")
@@ -50,18 +48,8 @@ public class SearchTests extends TestBase{
     @DisplayName("Open jenkins job by unsigned user")
     void negativeJenkinsTest() {
         step("Open jenkins job", () -> {
-            final String regex = "[SEVERE]";
-            final Pattern pattern = Pattern.compile(regex, Pattern.MULTILINE);
             open("https://jenkins.autotests.cloud/view/Group%203/job/sergeyr_testInBrowser/");
-            final Matcher matcher = pattern.matcher(getBrowserConsoleLogs());
-            //how to verify that consoleLogs does not contain [SEVERE]
-            System.out.println(matcher);
-            while (matcher.find()) {
-                System.out.println("Full match: " + matcher.group(0));
-                for (int i = 1; i <= matcher.groupCount(); i++) {
-                    System.out.println("Group " + i + ": " + matcher.group(i));
-                }
-            }
+            assertFalse(getBrowserConsoleLogs().contains("[SEVERE]"));
         });
     }
 }
