2c41c51c6a400ba9c075cf2c28031e40c6ca9bcd
sergeyrudov <255400warr>
2020-09-14 19:39:56 +0300

    - update HW

--- src/test/java/tests/HW6/SearchTests.java
+++ src/test/java/tests/HW6/SearchTests.java
@@ -9,7 +9,6 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
 import pages.GooglePage;
-import tests.HW7.TestBase;
 
 import static com.codeborne.selenide.Selenide.open;
 import static io.qameta.allure.Allure.step;
