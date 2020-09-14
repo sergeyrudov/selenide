d27614d200bade7cf1f690fcba44766612798227
sergeyrudov <255400warr>
2020-09-14 19:27:11 +0300

    - update HW

--- src/test/java/tests/HW7/TestBase.java
+++ src/test/java/tests/HW7/TestBase.java
@@ -8,36 +8,34 @@ import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
 import tests.HW7.drivers.CustomWebDriver;
 
-import static com.codeborne.selenide.Selenide.closeWebDriver;
+import static com.codeborne.selenide.WebDriverRunner.closeWebDriver;
 import static com.codeborne.selenide.logevents.SelenideLogger.addListener;
-import static io.qameta.allure.selenide.LogType.BROWSER;
-import static io.qameta.allure.selenide.LogType.CLIENT;
-import static tests.HW6.helpers.AttachmentsHelper.*;
+import static org.openqa.selenium.logging.LogType.BROWSER;
+import static tests.HW7.helpers.AttachmentsHelper.*;
 
 
-public class TestBase {
+class TestBase {
+
     @BeforeAll
     @Step("Tests setup")
     public static void beforeAll() {
-        addListener("AllureSelenide", new AllureSelenide()
-                .screenshots(true)
-                .savePageSource(true));
-        System.setProperty("selenoid_url", "https://user1:1234@selenoid.autotests.cloud:4444/wd/hub/");
-
+        addListener("AllureSelenide", new AllureSelenide().screenshots(true).savePageSource(true));
         Configuration.browser = CustomWebDriver.class.getName();
     }
 
     @AfterEach
     @Step("Attachments")
-    public void afterEach() {
+    public void afterEach(){
         attachScreenshot("Last screenshot");
         attachPageSource();
         attachAsText("Browser console logs", getBrowserConsoleLogs());
+        attachVideo();
 
         closeWebDriver();
     }
 
     public static String getBrowserConsoleLogs() {
-        return String.join("\n", Selenide.getWebDriverLogs(String.valueOf(BROWSER)));
+        return String.join("\n", Selenide.getWebDriverLogs(BROWSER));
     }
+
 }
